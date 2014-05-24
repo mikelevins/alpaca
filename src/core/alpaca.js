8df15e46fa13be4e59fc3fa200df34319e046d82
mikel evins <mevins@me.com>
2014-05-24 01:04:35 -0500

    built the File menu

--- src/core/alpaca.js
+++ src/core/alpaca.js
@@ -1,13 +1,59 @@
-// menu
-var gui = require('nw.gui');
-var menu = new gui.Menu();
+// alpaca module
 
-menu.append(new gui.MenuItem({
-    label: 'Alpaca Test',
-    click: function () {alert('chose Alpaca Test')}}));
+var ALPACA = (function (){
+    var alpaca = {};
 
-var menubar = new gui.Menu({type: 'menubar'});
-menubar.append(new gui.MenuItem({label: 'Testing', submenu: menu}));
+    // -------------------------
+    // app object
+    // -------------------------
+    
+    alpaca.gui = require('nw.gui');
+    alpaca.win = alpaca.gui.Window.get();
 
-var win = gui.Window.get();
-win.menu = menubar;
+    // -------------------------
+    // menubar
+    // -------------------------
+
+    alpaca.menubar = new alpaca.gui.Menu({ type: 'menubar' });
+
+    // -------------------------
+    // File menu
+    // -------------------------
+
+    alpaca.fileMenu = new alpaca.gui.Menu();
+    alpaca.fileMenu.append(new alpaca.gui.MenuItem({
+        label: 'New',
+        click: function () {alert('New file')}}));
+    alpaca.fileMenu.append(new alpaca.gui.MenuItem({
+        label: 'Open...',
+        click: function () {alert('Open a file')}}));
+    alpaca.fileMenu.append(new alpaca.gui.MenuItem({
+        label: 'Directory',
+        click: function () {alert('Dired')}}));
+    alpaca.fileMenu.append(new alpaca.gui.MenuItem({
+        label: 'Close',
+        click: function () {alert('Close the file')}}));
+    alpaca.fileMenu.append(new alpaca.gui.MenuItem({
+        label: 'Save',
+        click: function () {alert('Save the file')}}));
+    alpaca.fileMenu.append(new alpaca.gui.MenuItem({
+        label: 'Save As...',
+        click: function () {alert('Save the file as...')}}));
+
+    
+    // -------------------------
+    // Help menu
+    // -------------------------
+
+    alpaca.helpMenu = new alpaca.gui.Menu();
+
+    // -------------------------
+    // menubar setup
+    // -------------------------
+
+    alpaca.win.menu = alpaca.menubar;
+    alpaca.win.menu.insert(new alpaca.gui.MenuItem({ label: 'File', submenu: alpaca.fileMenu}), 1);
+    alpaca.win.menu.append(new alpaca.gui.MenuItem({ label: 'Help', submenu: alpaca.helpMenu}));
+
+    return alpaca;
+})();
--- src/core/init.js
+++ src/core/init.js
@@ -1,7 +1,11 @@
 // CodeMirror init
+
+// configuration
 var cmConfig = {
-mode: 'markdown',
-theme: 'twilight'
+    mode: 'markdown',
+    theme: 'twilight',
+    lineNumbers: true
 };
+
 var theTextArea = document.getElementById('theEditorTextArea');
 var theCodeMirror = CodeMirror.fromTextArea(theTextArea,cmConfig);

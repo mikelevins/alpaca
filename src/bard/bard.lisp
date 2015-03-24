d0fd02f2e07b664149d4afd946462a109c52f26d
mikel evins <mevins@me.com>
2015-03-24 00:26:25 -0500

    set up framework for initializing bard runtime

--- src/bard/bard.lisp
+++ src/bard/bard.lisp
@@ -10,3 +10,21 @@
 
 (in-package :bard-internal)
 
+(defparameter $bard-banner (format nil "bard ~a" *bard-version-number*))
+
+(defun display-bard-prompt ()
+  (format t "bard> "))
+
+(defun bard-print (obj)
+  )
+
+(defun repl ()
+  (format t "~%~a" $bard-banner)
+  (init-bard-globals)
+  (catch 'exit-bard
+    (loop
+       (display-bard-prompt)
+       (let* ((input (bard-read))
+              (thunk (compile input (empty-environment))))
+         (bard-print thunk)
+         (terpri)))))
---
+++ src/bard/init.lisp
@@ -0,0 +1,31 @@
+;;;; ***********************************************************************
+;;;;
+;;;; Name:          init.lisp
+;;;; Project:       Bard
+;;;; Purpose:       initialize the bard environment before starting a repl
+;;;; Author:        mikel evins
+;;;; Copyright:     2015 mikel evins
+;;;;
+;;;; ***********************************************************************
+
+(in-package :bard-internal)
+
+(defun init-bard-globals ()
+  (global-set! 'bard::|character| |character|)
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  ;; (global-set! )
+  )

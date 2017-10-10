a4d96c89905e5489cfa55f5d73ff7595f4780604
mikel evins <mevins@me.com>
2017-10-10 08:50:04 -0500

    Added eidt and font menus to test editor

--- src/alpaca.rkt
+++ src/alpaca.rkt
@@ -5,9 +5,15 @@
                       [width 200]
                       [height 200]))
          (canvas (new editor-canvas% [parent frame]))
-         (text (new text%)))
+         (text (new text% [auto-wrap #t]))
+         (menubar (new menu-bar% [parent frame]))
+         (edit-menu (new menu% [label "Edit"] [parent menubar]))
+         (font-menu (new menu% [label "Font"] [parent menubar])))
+    (append-editor-operation-menu-items edit-menu #f)
+    (append-editor-font-menu-items font-menu)
     (send canvas set-editor text)
     (send frame show #t)
+    (send text set-max-undo-history 100)
     frame))
 
 ;;; (define $ed (new-editor))

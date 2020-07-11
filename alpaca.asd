9003d1d4ff2744c16232cb19e13b14173af81e0d
mikel evins <mevins@me.com>
2020-07-11 14:53:09 -0500

    began simple customizations of the example qtools code

--- alpaca.asd
+++ alpaca.asd
@@ -1,10 +1,11 @@
 ;;;; alpaca.asd
 
 (asdf:defsystem #:alpaca
-  :description "Describe alpaca here"
-  :author "Your Name <your.name@example.com>"
-  :license  "Specify license here"
-  :version "0.0.1"
+  :description "Alpaca: a programmable editor for authors"
+  :author "mikel evins <mikel@evins.net>"
+  :license  "Apache 2.0"
+  :version "0.9.1"
+  :homepage "https://github.com/mikelevins/alpaca"
   :serial t
   :depends-on (:qtools :qtcore :qtgui)
   :components ((:module "src"
@@ -14,3 +15,4 @@
 
 
 ;;; (asdf:load-system :alpaca)
+;;; (alpaca:main)
--- src/alpaca.lisp
+++ src/alpaca.lisp
@@ -3,5 +3,29 @@
 (in-package #:alpaca)
 (in-readtable :qtools)
 
+(defparameter *version-string* "v0.9.1d1")
 
+(define-widget main (QWidget)
+  ())
+
+(define-subwidget (main button) (q+:make-qpushbutton "Alpaca Info" main))
+
+(define-subwidget (main layout) (q+:make-qhboxlayout main)
+  (q+:add-widget layout button))
+
+(define-slot (main button-pressed) ()
+  (declare (connected button (released)))
+  (q+:qmessagebox-information
+   main
+   (format nil "Alpaca ~A" *version-string*)
+   (format NIL
+           "Welcome to Alpaca. ~
+A work in progress.
+Running ~a v~a on ~a."
+           (lisp-implementation-type)
+           (lisp-implementation-version)
+           (machine-type))))
+
+(defun main ()
+  (with-main-window (window (make-instance 'main))))
 
